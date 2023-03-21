1595f4dc5ea2c45c09f44522ff450c24a7a6cf31
wildmanwang <cliff.w@qq.com>
2023-03-21 16:21:41 +0800

    code back up

--- sales_manager_front/src/api/user.js
+++ sales_manager_front/src/api/user.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/admin/login',
+    url: '/login',
     method: 'post',
     data
   })
@@ -10,7 +10,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/admin/userinfo',
+    url: '/userinfo',
     method: 'get',
     params: { token }
   })
@@ -18,7 +18,7 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/admin/logout',
+    url: '/logout',
     method: 'post'
   })
 }
